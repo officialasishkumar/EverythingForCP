7d2337bc51f7b4f09d7215f86030315010c9439c
Asish Kumar <officialasishkumar@gmail.com>
2025-02-23 19:39:30 +0530

    algo viz

---
+++ frontend/src/algoviz/AlgovizApp.jsx
@@ -0,0 +1,28 @@
+import HomePage from "./Components/HomePage";
+import './Styles/home-page.css';
+import './Styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Tree from "./Algorithms/Tree/Tree";
+import Array from "./Algorithms/Array/Array";
+import Stack from "./Algorithms/Stack/Stack";
+import LinkedList from "./Algorithms/LinkedList/LinkedList";
+import Animation from "./Algorithms/LinkedList/Animation";
+import Matrix from "./Algorithms/Matrix/Matrix";
+import WorkInProgress from "./Components/WorkInProgress";
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tree" element={<Tree />} />
+        <Route path="/array/*" element={<Array />} />
+        <Route path="/stack" element={<Stack />} />
+        <Route path="/linked-list" element={<LinkedList />} />
+        <Route path="/linked-list/animation" element={<Animation />} />
+        <Route path="/matrix" element={<Matrix />} />
+        <Route path="*" element={<WorkInProgress />}></Route>
+      </Routes>
+  );
+}
+
+export default App;
